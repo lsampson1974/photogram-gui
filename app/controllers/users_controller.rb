051882c997c6487f17e6f35e173a28e92afdf512
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-02-13 22:35:07 +0000

    List user screen completed - no links working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   
     def list_all_users
 
+      @users = User.all.order({ :username => :asc }) 
+
       render({ :template => "layouts/users_list" })
 
     end # Of method.
